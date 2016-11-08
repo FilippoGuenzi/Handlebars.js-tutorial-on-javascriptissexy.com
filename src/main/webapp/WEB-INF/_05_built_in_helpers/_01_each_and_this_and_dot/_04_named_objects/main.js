2d72293f52d07742bbe749df23ab702545d610d5
fguenzi <fguenzi@egencia.com>
2016-11-08 09:04:00 +0100

    Built-in helpers - each suite

---
+++ src/main/webapp/WEB-INF/_05_built_in_helpers/_01_each_and_this_and_dot/_04_named_objects/main.js
@@ -0,0 +1,20 @@
+$(
+    function () {
+
+        var model = {customer1: {firstName: 'Marc', lastName: 'Furlain'}, customer2: {firstName: 'Paul', lastName: 'Snash'}};
+
+        var thereTemplateContent = $("#there").html();
+        var there2TemplateContent = $("#there2").html();
+        var there3TemplateContent = $("#there3").html();
+        var there4TemplateContent = $("#there4").html();
+        var thereTemplateFunction = Handlebars.compile(thereTemplateContent);
+        var there2TemplateFunction = Handlebars.compile(there2TemplateContent);
+        var there3TemplateFunction = Handlebars.compile(there3TemplateContent);
+        var there4TemplateFunction = Handlebars.compile(there4TemplateContent);
+
+        $(document.body).append(thereTemplateFunction(model));
+        $(document.body).append(there2TemplateFunction(model));
+        $(document.body).append(there3TemplateFunction(model));
+        $(document.body).append(there4TemplateFunction(model));
+    }
+);
