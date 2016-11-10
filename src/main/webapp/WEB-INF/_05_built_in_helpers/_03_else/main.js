6edbbca8dc0ab35de9cb279c093c3a4d094845bf
fguenzi <fguenzi@egencia.com>
2016-11-10 08:46:18 +0100

    Built-in helpers - unless

---
+++ src/main/webapp/WEB-INF/_05_built_in_helpers/_03_else/main.js
@@ -0,0 +1,25 @@
+$(
+    function () {
+
+        var model = {
+            variable1: {}
+            , variable2: "yes"
+            , variable3: 3
+            , variable4: true
+            , variable5: [2, 3, 7]
+            , variable6: ""
+            , variable7: 0
+            , variable8: false
+            , variable9: []
+            , variable10: null
+            , variable11: undefined
+            , variable12: NaN
+        };
+
+        var templateContent = $("#tplt").html();
+        var templateFunction = Handlebars.compile(templateContent);
+
+        $(document.body).append(templateFunction(model));
+
+    }
+);
